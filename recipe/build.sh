7c8a69f8c70d50c7127cd83693a3ad728a8eb683
Onur Bingol <obingol@anaconda.com>
2026-03-25 15:06:52 -0700

    Anaconda specific changes

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,6 +18,25 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
     fi
 fi
 
+# Why NVCC_APPEND_FLAGS?
+#
+#  NVCC resolves conflicting flags by using the last value. We use NVCC_APPEND_FLAGS
+#  to ensure our flags come after those set in NCCL's Makefile.
+#
+# Added flags:
+#
+#  -std=c++17: GCC 14's libstdc++ headers use C++17 inline variables and built-in traits
+#    that NVCC's EDG frontend cannot parse unless host compilation is also C++17.
+#    NCCL's Makefile hardcodes -std=c++11, so addition of this flag produces a harmless warning:
+#    "nvcc warning : incompatible redefinition for option 'std', the last value of this option was used"
+#    Reference: https://github.com/NVIDIA/nccl/blob/v2.25.1-1/makefiles/common.mk
+#
+#  -Wno-deprecated-gpu-targets: Suppress warnings about deprecated GPU targets
+#    (sm_50, sm_52, etc.) included in NVCC_GENCODE for broad compatibility.
+export NVCC_APPEND_FLAGS="${NVCC_APPEND_FLAGS} -std=c++17 -Wno-deprecated-gpu-targets"
+
+# Additional flags to make command:
+#  -j${CPU_COUNT}: Build with the number of CPUs specified by the CPU_COUNT environment variable.
 make -j${CPU_COUNT} src.lib
 
 make install PREFIX="${PREFIX}"
